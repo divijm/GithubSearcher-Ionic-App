f9a85f5d9bc19bdce024a90266b5ef77f6eb49d3
Divij <divij.mehra.16@gmail.com>
2017-06-19 21:31:02 +1000

    cordova build stage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { HttpModule } from "@angular/http";
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { UsersPage } from '../pages/users/users';
@@ -9,7 +9,6 @@ import { ReposPage } from '../pages/repos/repos';
 import { OrganisationsPage } from '../pages/organisations/organisations'
 
 import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
 import { GithubUsers } from '../providers/github-users/github-users';
 import { UserDetailsPage } from '../pages/user-details/user-details';
 
